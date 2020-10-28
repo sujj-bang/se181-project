0b473a5c5a5686df57065015e4cfff84473f5241
amankartha <32320021+amankartha@users.noreply.github.com>
2020-10-28 08:37:59 +0400

    fixed issue

--- checkers/piece.py
+++ checkers/piece.py
@@ -8,17 +8,14 @@ class Piece:
         self.col = col
         self.color = color
         self.king = False
-        
-        if self.color == RED:
-            self.direction = -1
-        else:
-            self.direction = 1
-        
-        self.x = 0
-        self.y = 0
+        #if self.color == RED:
+       #     self.direction = -1
+     #   else:
+     #       self.direction = 1
+        self.calc_pos()
         
     def calc_pos(self):
-        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE //2
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE //2 
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE //2
         
     def make_king(self):
