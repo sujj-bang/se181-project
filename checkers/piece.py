1f8511b58a76cae8351379b4174153876f8e230c
Su Jin Bang <sjb443@drexel.edu>
2020-11-23 23:06:37 -0500

    reformat files

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,9 +1,12 @@
 import pygame
-from .constants import RED, WHITE, GREY,SQUARE_SIZE,CROWN
+from .constants import RED, WHITE, GREY, SQUARE_SIZE, CROWN
+
+
 class Piece:
     PADDING = 15
-    OUTLINE = 2 
-    def __init__(self,row, col, color):
+    OUTLINE = 2
+
+    def __init__(self, row, col, color):
         self.row = row
         self.col = col
         self.color = color
@@ -11,24 +14,25 @@ class Piece:
         self.x = 0
         self.y = 0
         self.calc_pos()
-        
+
     def calc_pos(self):
-        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE //2 
-        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE //2
-        
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
     def make_king(self):
         self.king = True
-    
-    def move(self,row,col):
+
+    def move(self, row, col):
         self.row = row
         self.col = col
         self.calc_pos()
-        
+
     def draw(self, win):
-        radius = SQUARE_SIZE//2 - self.PADDING
-        pygame.draw.circle(win, GREY, (self.x,self.y), radius + self.OUTLINE)
-        pygame.draw.circle(win,self.color, (self.x,self.y), radius)
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
         if self.king:
-            win.blit(CROWN, (self.x - CROWN.get_width()//2,self.y - CROWN.get_height()//2))
+            win.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
+
     def __repr__(self):
         return str(self.color)
